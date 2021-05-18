55cb3a2052aced0c8a87db759537c9c1fddff686
walidrafeii <70703445+walidrafeii@users.noreply.github.com>
2021-05-18 13:39:36 -0700

    Delete Pods directory

--- Pods/Target Support Files/Pods-FlowerClassification-FlowerClassificationUITests/Pods-FlowerClassification-FlowerClassificationUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_FlowerClassification_FlowerClassificationUITests : NSObject
-@end
-@implementation PodsDummy_Pods_FlowerClassification_FlowerClassificationUITests
-@end
--- Pods/Target Support Files/Pods-FlowerClassification/Pods-FlowerClassification-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_FlowerClassification : NSObject
-@end
-@implementation PodsDummy_Pods_FlowerClassification
-@end
